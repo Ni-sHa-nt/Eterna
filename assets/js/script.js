82866a219e070c81c1ee9b0ace4d4fb53c7b1970
Ni-sHa-nt <nk4815027@gmail.com>
2026-01-04 19:50:48 +0530

    For every you section responsive amd some changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,6 +144,7 @@ setInterval(updateCounter, 60000); // update every minute
 
 
 
+// For Every You Section
 const forEverySwiper = new Swiper(".forEverySwiper", {
     slidesPerView: 3,
     spaceBetween: 10,
@@ -153,10 +154,10 @@ const forEverySwiper = new Swiper(".forEverySwiper", {
     grabCursor: true,
 
 
-    // autoplay: {
-    //     // delay: 1,
-    //     // disableOnInteraction: false,
-    // },
+    autoplay: {
+        // delay: 1,
+        // disableOnInteraction: false,
+    },
 
     navigation: {
         nextEl: ".swiper-button-next",
@@ -174,82 +175,6 @@ const forEverySwiper = new Swiper(".forEverySwiper", {
 
 
 
-// Shop The Look Section
-// $(document).ready(function () {
-
-//     const slider = $(".myslider");
-
-//     slider.owlCarousel({
-//         items: 1,
-//         loop: false,
-//         autoplay: false,
-//         autoplayHoverPause: true,
-//         autoplayTimeout: 5000,
-//         smartSpeed: 1000,
-//         margin: 0,
-//         nav: true,
-//         dots: false,
-//         lazyLoad: true,
-//         responsive: {
-//             0: { items: 1, nav: false, dots: true },
-//             360: { items: 1, nav: false, dots: true },
-//             480: { items: 1, nav: false, dots: true },
-//             600: { items: 1, nav: false, dots: true },
-//             768: { items: 1, nav: false, dots: true },
-//             992: { items: 1, nav: false, dots: true },
-//             1200: { items: 1, nav: true, dots: false },
-//         },
-//     });
-
-//     // ✅ Total slides (excluding cloned)
-//     const totalSlides = slider.find('.owl-item:not(.cloned)').length;
-//     $('#total').text(totalSlides);
-
-//     // ✅ Current slide number
-//     slider.on('changed.owl.carousel', function (event) {
-//         $('#current').text(event.item.index + 1);
-//     });
-
-// });
-
-
-// =================Marquee Slider Start===================== //
-// $(function () {
-//     $('.marquee-carousel').owlCarousel({
-//         loop: true,
-//         autoplay: true,
-//         // slideTransition: 'linear',
-//         // autoplaySpeed: 2000,
-//         autoplayTimeout: 2500,
-//         smartSpeed: 2500,
-//         // margin: 60,
-//         nav: false,
-//         dots: false,
-//         navElement: 'div',
-//         // navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-//         lazyLoad: true,
-//         responsive: {
-//             0: {
-//                 items: 3,
-//                 loop: true
-//             },
-
-//             600: {
-//                 items: 4,
-//                 loop: true
-//             },
-//             768: {
-//                 items: 5,
-//                 loop: true
-//             },
-//             992: {
-//                 items: 6,
-//                 loop: true
-//             },
-//             1200: { items: 8 },
-//         },
-//     });
-// });
 
 
 
